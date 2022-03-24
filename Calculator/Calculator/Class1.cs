23e663dc3ec0daae89699f79bdbd4b0de1590717
236590 Maciaszek Konrad <236590@edu.p.lodz.pl>
2022-03-24 16:52:21 +0100

    Test

---
+++ Calculator/Calculator/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace Calculator
+{
+    public class CalculatorOperations
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Substract(double a, double b) 
+        { 
+            return a - b; 
+        }
+
+        public double Multiply(double a, double b) 
+        { 
+            return a * b; 
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b != 0)
+                throw new DivideByZeroException();
+            return a / b;
+        }
+
+    }
+}
---
+++ Calculator/TestCalculator/UnitTest1.cs
@@ -0,0 +1,70 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+
+namespace TestCalculator
+{
+    [TestClass]
+    public class CalculatorOperationsTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            double a = 2;
+            double b = 2;
+
+            CalculatorOperations operations = new CalculatorOperations();
+            
+            double c = operations.Add(a, b);    
+
+            Assert.AreEqual(4, c);
+        }
+
+        [TestMethod]
+        public void TestSubstract()
+        {
+            double a = 4;
+            double b = 2;
+
+            CalculatorOperations operations2 = new CalculatorOperations();
+            
+            double c = operations2.Substract(a, b);
+
+            Assert.AreEqual(2, c);
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            double a = 4;
+            double b = 2;
+
+            CalculatorOperations operations = new CalculatorOperations();
+
+            double c = operations.Multiply(a, b);
+
+            Assert.AreEqual(8, c);
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            double a = 4;
+            double b = 2;
+
+            CalculatorOperations operations = new CalculatorOperations();
+
+            try
+            {
+                double c = operations.Divide(a, b);
+                Assert.AreEqual(2, c);
+            }
+            catch (System.DivideByZeroException e)
+            {
+
+            }
+        }
+
+
+    }
+}
