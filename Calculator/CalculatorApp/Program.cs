50eb806bf49d0b11cccc5a827e0d2ddaf58716a6
Gharte15 <70962125+Gharte15@users.noreply.github.com>
2022-04-21 16:26:54 +0200

    Dodano warstwy logiki, danych i MVVM

---
+++ Calculator/CalculatorApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Calculator;
+
+Console.WriteLine("[1] Dodawanie");
+Console.WriteLine("[2] Odejmowanie");
+Console.WriteLine("[3] Mnozenie");
+Console.WriteLine("[4] Dzielenie");
+Console.Write("Wybor: ");
+
+int choice = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Podaj pierwsza liczbe: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Podaj druga liczbe: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+CalculatorOperations operations = new CalculatorOperations();
+
+switch (choice)
+{
+    case 1:
+        Console.WriteLine("a + b = " + operations.Add(a, b));
+        break;
+    case 2:
+        Console.WriteLine("a - b = " + operations.Substract(a, b));
+        break;
+    case 3:
+        Console.WriteLine("a * b = " + operations.Multiply(a, b));
+        break;
+    case 4:
+        Console.WriteLine("a / b = " + operations.Divide(a, b));
+        break;
+    default:
+        Console.WriteLine("Nieznana operacja");
+        break;
+}
