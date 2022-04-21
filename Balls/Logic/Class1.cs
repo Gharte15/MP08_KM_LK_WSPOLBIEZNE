fc91596bbfdf28f2dc5d227224e8a86a1d7bdf47
lkrol8006 <l.krol8006@gmail.com>
2022-04-21 19:13:26 +0200

    Data i Logic

--- Balls/Logic/Class1.cs
+++ Balls/Logic/Class1.cs
@@ -1,7 +1,37 @@
-﻿namespace Logic
-{
-    public class Class1
+﻿namespace Logic {
+using Data;
+    public class BallController
     {
+        public BallController()
+        {
+        }
+
+        public Ball [] balls = new Ball[20];
+        public Ball createBall(int r, int x, int y)
+        {
+            Ball ball = new Ball(r, x, y);
+            return ball;
+        }
+
+        public void addBall(Ball ball)
+        {
+            balls.Append(ball);
+        }
+
+        public int changeCoordinate(int c)
+        {
+            Random random = new Random();
+            int a = random.Next(c - 10, c + 10);
+            return a;
+        }
+
+        public void moveBall(Ball ball)
+        {
+            ball.x = changeCoordinate(ball.x);
+            ball.y = changeCoordinate(ball.y);
+        }
+
+
 
     }
 }
