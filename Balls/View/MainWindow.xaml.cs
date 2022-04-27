124a79c7a9b09e43ca07f49cdb9c4855f1382d18
Gharte15 <70962125+Gharte15@users.noreply.github.com>
2022-04-28 01:44:29 +0200

    Nadal nie dziala

--- Balls/View/MainWindow.xaml.cs
+++ Balls/View/MainWindow.xaml.cs
@@ -12,9 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Media.Animation;
 
 namespace View
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -22,7 +25,37 @@ namespace View
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();           
+        }
+
+        public void buttonStartClicked(object sender, EventArgs e)
+        {
+            draw.circle(400, 400, 200, 200, MainCanvas);
+            draw.circle(750, 600, 200, 200, MainCanvas);
+            draw.circle(900, 900, 200, 200, MainCanvas);
+        }
+
+        class draw
+        {
+            public static void circle(int x, int y, int width, int height, Canvas cv)
+            {
+                Ellipse circle = new Ellipse()
+                {
+                    Width = width,
+                    Height = height,
+                    Stroke = Brushes.Red,
+                    Fill = Brushes.Red,
+                    StrokeThickness = 6
+                };
+
+                cv.Children.Add(circle);
+
+                circle.SetValue(Canvas.LeftProperty, (double)x);
+                circle.SetValue(Canvas.TopProperty, (double)y);
+
+            }
         }
     }
+
+
 }
---
+++ Balls/ViewModel/ViewModelBalls.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using Model;
+
+namespace ViewModel
+{
+    class ViewModelBalls : INotifyPropertyChanged
+    {
+        private Ball ball;
+        //private Ball [] balls = new Ball [Model.Ball.NumberOfBalls]
+
+    public ViewModelBalls()
+    {
+        ball = new Ball(50, 50, 20);
+    }
+
+    public int X
+        {
+            get { return ball.X; }
+            set { ball.X = value; }
+        }
+    public int Y
+        {
+            get { return ball.Y; }
+            set { ball.Y = value; }
+        }
+
+    public int R
+        {
+            get { return ball.R; }
+            set { ball.R = value; }
+        }
+
+    public Ball createBall(int x, int y, int r)
+        {
+            Ball ball = new Ball(x, y, r);
+            return ball;
+        }
+
+    public void moveBallRandomly()
+    {
+        var ran = new Random();
+        ball.X = ran.Next(5);
+        ball.Y = ran.Next(5);
+    }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+}
