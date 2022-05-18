073d2a5644e0c45a93e9dc9bf3c3ed799c8117ab
lkrol8006 <l.krol8006@gmail.com>
2022-05-18 18:14:56 +0200

    Ball Taski

--- Balls/Data/Ball.cs
+++ Balls/Data/Ball.cs
@@ -1,54 +1,80 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace Data
 {
-    public class Ball
+    public interface IBall : INotifyPropertyChanged
     {
-        private int x, y, r, weight;
-        public int X 
-        { 
+        int X { get; set; }
+        int Y { get; set; }
+        int R { get;}
+        int Weight { get; }
+
+        int Identifier { get; }
+        void Move();
+        void Reset();
+        void CreateTask(int period);
+    }
+
+    internal class Ball : IBall
+    {
+        private int x;
+        private int y;
+        private readonly int r;
+        private readonly int weight;
+        private readonly int identifier;
+        private readonly Stopwatch stopwatch = new Stopwatch();
+        private Task task;
+        private bool stop = false;
+
+        public Ball(int id, int x, int y, int r, int weight)
+        {
+            identifier = id;
+            this.x = x;
+            this.y = y;
+            this.r = r;
+            this.weight = weight;
+        }
+
+        public int Identifier { get => identifier; }
+        public int X
+        {
             get => x;
             set
             {
+                if (value.Equals(x))
+                {
+                    return;
+                }
+
                 x = value;
+                RaisePropertyChanged(nameof(X));
             }
         }
-
         public int Y
         {
             get => y;
             set
             {
+                if (value.Equals(y))
+                {
+                    return;
+                }
+
                 y = value;
+                RaisePropertyChanged(nameof(Y));
             }
         }
 
-        public int R
+        public int R { get => r; }
+        public int Weight { get => weight; }
+        public void Move()
         {
-            get => r;
-            set
-            {
-                r = value;
-            }
-        }
-
-        public Ball(int x, int y, int r)
-        {
-            this.x = x;
-            this.y = y;
-            this.r = r;
-            
-        }
-
-        public int Weight
-        {
-            get => weight;
-            set
-            {
-                weight = value;
-            }
+            X = x + R;
+            Y = y + R;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -57,6 +83,33 @@ namespace Data
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        public void CreateTask(int period)
+        {
+            stop = false;
+            task = Run(period);
+        }
+
+        private async Task Run(int period)
+        {
+            while (!stop)
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+                if (!stop)
+                {
+                    Move();
+                    RaisePropertyChanged();
+                }
+                stopwatch.Stop();
+
+                await Task.Delay((int)(period - stopwatch.ElapsedMilliseconds));
+            }
+        }
+        public void Reset()
+        {
+            stop = true;
+        }
+
 
     }
 }
